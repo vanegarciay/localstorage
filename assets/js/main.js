98bb3c1d0777380209aa8726f2a49c4b7b819283
VaneGarciaYara <vanegarciay@gmail.com>
2017-06-16 12:22:47 -0400

    estilos y html terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-alert("holi");
+function guardarDatos() {
+    localStorage.nombre = document.getElementById("nombre").value;
+    localStorage.password = document.getElementById("password").value;
+}
+
+function recuperarDatos() {
+    if ((localStorage.nombre != undefined) && (localStorage.password != undefined)) {
+        
+    }
+}
